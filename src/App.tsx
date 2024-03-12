a4e5ae98dca5c3c27e78fbdccbb671d3d35a2ce0
Paulo Rodrigues <P39210@parceirosedu.com.br>
2024-03-12 18:19:01 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <nav className='bg-red-800  '>
+        <p className='text-xl text-red-950'>
+          XPTOOOOOOOOOOOOOOOOOOOOOO
+        </p>
+      </nav>
+    </div>
+  )
+}
+
+export default App
